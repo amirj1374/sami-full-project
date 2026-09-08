35aba50b0f7435ba8934e2c26518cd3d7d7d62d3
jalili_a <jalili@test.com>
2026-09-08 14:27:24 +0330

    feat(purchasing): complete supplier invoice payable workflow

---
+++ sami-frontend/src/api/supplierInvoices.ts
@@ -0,0 +1,2 @@
+import {http,unwrap} from './http'
+export const supplierInvoicesApi={list:()=>unwrap<any[]>(http.get('/api/v1/supplier-invoices')),get:(id:number)=>unwrap<any>(http.get(`/api/v1/supplier-invoices/${id}`)),create:(p:any)=>unwrap<any>(http.post('/api/v1/supplier-invoices',p.lines,{params:{companyId:p.companyId,branchId:p.branchId,purchaseOrderId:p.purchaseOrderId,goodsReceiptId:p.goodsReceiptId}}))}
